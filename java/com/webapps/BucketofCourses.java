5a2f4fe666f388483c31e4129f7a77cec6fdf50d
sysadmin <sysadmin@dp3551-u2004.csez.zohocorpin.com>
2022-11-27 23:31:19 +0530

    commit buket courses

--- java/com/webapps/BucketofCourses.java
+++ java/com/webapps/BucketofCourses.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//
 public class BucketofCourses extends HttpServlet {
 	public void service(HttpServletRequest request, HttpServletResponse response)
 		      throws ServletException, IOException {
